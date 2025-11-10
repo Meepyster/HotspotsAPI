c8c859b0193043f989bce478d286f6f71452f1ad
Meepyster <iforlemu2876@gmail.com>
2025-11-10 21:23:18 +0000

    initial commit for hotspotsAPI !

---
+++ main.py
@@ -0,0 +1,15 @@
+import asyncio
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.post("/connect/test")
+async def emulate_connection():
+    await asyncio.sleep(5)
+    return {"status": "connected", "message": "Connection successful"}
+
+
+@app.get("/location/test/pit")
+async def get_chapel_hill_location():
+    return {"name": "The Old Well", "latitude": 35.9101, "longitude": -79.0486}
